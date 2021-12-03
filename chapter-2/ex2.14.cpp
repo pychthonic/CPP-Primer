e4417a5d7c4772d5daafb9b1dbee2190add11c97
pychthonic <fridgewolf@protonmail.com>
2021-12-02 19:40:55 -0500

    fixed ex2.14

--- chapter-2/ex2.14.cpp
+++ chapter-2/ex2.14.cpp
@@ -2,10 +2,13 @@
 
 
 int main() {
-    int ival = 1.01; // legal
-    // int &rval1 = 1.01; // ERROR IF UNCOMMENTED: cannot assign a reference to a literal
-    int &rval2 = ival; // legal
-    // int &rval3; // ERROR IF UNCOMMENTED: references must be initialized
+	int i = 100, sum = 0;
 
-    return 0;
+	for (int i = 0; i != 10; ++i)
+		sum += i;
+	std::cout << i << " " << sum << std::endl;
+	
+	// legal, just convoluted. it will print: 100 45
+
+	return 0;
 }
