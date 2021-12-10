b166fcb371a570c2921d2e61cb888857851eb75a
pychthonic <fridgewolf@protonmail.com>
2021-12-10 13:34:23 -0500

    finished too much exercises

---
+++ chapter-2/ex2.40.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct Sales_data {
+	std::string bookTitle;
+	std::string bookISBN;
+	double bookPrice;
+	unsigned inventoryCount;
+};
+
+int main() {
+	Sales_data book1;
+
+	std::cout << "\nTitle: ";
+	std::getline(std::cin, book1.bookTitle);
+	std::cout << "ISBN: ";
+	std::cin >> book1.bookISBN;
+	std::cout << "Price: ";
+	std::cin >> book1.bookPrice;
+	std::cout << "How many copies of " << book1.bookTitle << " are there in stock? ";
+	std::cin >> book1.inventoryCount;
+
+	std::cout << "\nNew book entry:\n\nTitle: " << book1.bookTitle << "\nISBN: " << book1.bookISBN;
+	std::cout << "\nPrice: " << book1.bookPrice << "\nInventory Count: " << book1.inventoryCount << "\n\n";
+
+
+	return 0;
+}
