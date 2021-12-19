70ba3214d55aba2141997ac945ce0e5958600686
pychthonic <fridgewolf@protonmail.com>
2021-12-19 13:53:57 -0500

    weekend warrior

---
+++ chapter-3/ex3.26.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin, std::cout, std::endl, std::getline, std::string, std::vector;
+
+int main() {
+
+	// (beg + end) / 2 will possibly go past the bounds of the vector, even if dividing by two
+	// brings it back within the bounds, so this would result in undefined behavior.
+
+
+	return 0;
+}
