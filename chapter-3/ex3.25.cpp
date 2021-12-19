d4d9b1f070290e40f27d5106abc8935d368dbb72
pychthonic <fridgewolf@protonmail.com>
2021-12-19 13:49:16 -0500

    weekend warrior

--- chapter-3/ex3.25.cpp
+++ chapter-3/ex3.25.cpp
@@ -5,7 +5,25 @@
 using std::cin, std::cout, std::endl, std::getline, std::string, std::vector;
 
 int main() {
+	vector<unsigned> scores(11, 0);
+	unsigned grade;
+	while (cin >> grade) {
+		if (grade <= 100) {
+			auto it = scores.begin();
+			it += (grade / 10);
+			++(*it);
+		}
+	}
 
+	unsigned count = 0;
+	for (auto it = scores.begin(); it != scores.end(); ++it) {
+		if (count != 100) {
+			cout << count << "-" << count + 9 << ": " << *it << endl;
+			count += 10;
+		} else {
+			cout << count << ": " << *it << endl;
+		}
+	}
 
 
 
